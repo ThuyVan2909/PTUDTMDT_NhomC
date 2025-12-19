c9bf1a7289047918cff31c22833d85b82064ad54
31231026682@ueh.edu.vn <31231026682@ueh.edu.vn>
2025-12-19 20:49:45 +0700

    sửa button đăng nhập

--- index.php
+++ index.php
@@ -392,9 +392,16 @@ input.form-control:focus {
 
     <?php else: ?>
         <!-- Nếu đã đăng nhập -->
-        <a href="account.php" class="btn btn-outline-primary">
-            Tài khoản (<?= htmlspecialchars($userName) ?>)
-        </a>
+        <a href="account.php" class="btn btn-outline-primary account-btn d-flex align-items-center gap-2">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+         viewBox="0 0 16 16">
+        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3z"/>
+        <path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+    </svg>
+    <span class="account-name"><?= htmlspecialchars($userName) ?></span>
+</a>
+
+
     <?php endif; ?>
 
     <a href="cart.php" class="btn btn-outline-success position-relative">
